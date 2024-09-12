23dd49e33b2b1bedc7d90fc203b277e086ec7cdd
waledzee <waledzaki201@gmail.com>
2024-09-12 06:24:41 +0300

    bank class

---
+++ src/Bank.java
@@ -0,0 +1,32 @@
+public class Bank {
+    private Arraylist<Customer>customer;
+
+    public Bank()
+    {
+        customers = new ArrayList<>();
+    }
+    public void addCustomer(Customer customer)
+    {
+        Customer.add(customer);
+        System.out.println("customer added");
+    }
+    public void removeCustomerByID(String customerId)
+    {
+        for(Customer customer : customers)
+        {
+            if(customer.customerId.equals(customerId))
+            {
+                customers.remove(customer);
+            }
+        }
+    }
+    public void displayAllCustomers() {
+        if (customers.isEmpty()) {
+            System.out.println("No customers in the bank.");
+        } else {
+            for (Customer customer : customers) {
+                System.out.println(customer.displayCustomerInfo());
+            }
+        }
+    }
+}
