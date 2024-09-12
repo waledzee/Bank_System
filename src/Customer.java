78cbb1596724849acfbda6ec32cdd0ccb03cbc4e
waledzee <waledzaki201@gmail.com>
2024-09-12 06:07:01 +0300

    customer class

--- src/Customer.java
+++ src/Customer.java
@@ -1,3 +1,23 @@
 public class Customer {
+ public String name;
+ public String customerId;
+ public String[] contactInfo ;
+ private float balance;
+
+public Customer(String customerId,String name, String[] contactInfo,float balance)
+ {
+     this.customerId=customerId;
+     this.name=name;
+     this.contactInfo=contactInfo;
+     this.balance=balance;
+ }
+ public String getName() {
+        return name;
+}
+
+    public  String displayCustomerInfo()
+    {
+        return "Customer Info : name "+name+ " Customer Id is : "+customerId+" customer balance is " +balance;
+    }
 
 }
